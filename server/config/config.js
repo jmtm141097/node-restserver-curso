6327fef5ad74c825b0762a5a5619013edc2c45ec
Juan <jmtm141097@gmail.com>
2020-09-17 06:57:40 -0500

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,21 @@
 //===============
 
 process.env.PORT = process.env.PORT || 3000;
+
+//===============
+//entorno
+//===============
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//===============
+//base de datos
+//===============
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://doki2020:KnLGLG7nZXo5bG0d@cluster0.7mwfg.mongodb.net/cafe?retryWrites=true&w=majority';
+}
+
+process.env.URLDB = urlDB;
---
+++ server/models/usuario.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+let rolesValidos = {
+    values: ['ADMIN-ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol valido'
+};
+
+// usuario: doki2020
+// contra bd: KnLGLG7nZXo5bG0d
+
+// mongodb+srv://doki2020:KnLGLG7nZXo5bG0d@cluster0.7mwfg.mongodb.net/cafe
+
+let Schema = mongoose.Schema;
+
+let usuarioSchema = new Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es necesario']
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'El correo es necesario']
+    },
+    password: {
+        type: String,
+        required: [true, 'La contrasenia es obligatoria']
+    },
+    img: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        default: 'USER_ROLE',
+        enum: rolesValidos
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+});
+
+usuarioSchema.methods.toJSON = function() {
+
+    let user = this;
+
+    let userObject = user.toObject();
+
+    delete userObject.password;
+
+    return userObject;
+
+};
+
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser unico' });
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
